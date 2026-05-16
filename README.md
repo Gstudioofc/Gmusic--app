32fd8b84e934e117dccbaba12a5a9953f8a33426
Gstudioofc <gstudioapp900@gmail.com>
2026-05-16 14:49:06 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Gmusic--app
+# Gmusic
+App de música web PWA para gstudio store.
+
+## GitHub > Actions > APK
+1. Suba esta pasta para um repositório
+2. Actions roda automaticamente e gera app-debug.apk
+3. Baixe em Artifacts
---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('gmusic-v1').then(c=>c.addAll(['./','./index.html','./manifest.json','./assets/style.css','./assets/app.js','./assets/icon.png']))) });self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
